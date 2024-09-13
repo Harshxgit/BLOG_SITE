8a530afa6ad1ba06439d5902397fcee7b447bf45
Harshxgit <htp.codes@gmail.com>
2024-09-13 19:26:30 +0530

    created api

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,62 +1,65 @@
-import { Hono } from 'hono'
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
-import { sign } from 'hono/jwt'; 
-
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
 
 const app = new Hono<{
-	Bindings: {
-		DATABASE_URL: string
-    JWT_SECRET :string
-	}
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
 }>();
 
-app.get('/signup', async(c) => {
+app.get("/", (c) => {
+ 
+  return c.text("hello harshu");
+});
 
-  //connect client to database directly
-  const prisma = new PrismaClient({
-      datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate())
-
-
+//Signup
+app.post("/api/signup", async (c) => {
+  //only than prisma will talk to your database
   const body = await c.req.json();
 
-  try{
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate()); //this part is most important for pooling connection
+
+
+  try {
+    console.log("react")
     const user = await prisma.user.create({
-      data:{  
-        email : body.email,
-        password : body.password
-      }
-    })
-    const jwt = await sign({id :user.id}, c.env.JWT_SECRET) //payload in {}, and want jwt secret
-    return c.json({jwt})
-  } 
-  catch(e){
-     c.status(403)
-     return c.text("error while signing up")
+      //duplicate email exist
+      data: {
+        username: body.username,
+        password: body.password,
+        name: body.name,
+      },
+      
+    });
+    console.log("not reach")
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET); //payload in {}, and want jwt secret
+    return c.json({ jwt });
+  } catch (e) {
+    c.status(403);
+    return c.text("error while signing up");
   }
+});
 
-})
+app.post("/api/v1/", (c) => {
+  return c.text("user signup");
+});
 
+app.post("/api/v1/signin", (c) => {
+  return c.text("user signim");
+});
 
-app.post('/api/v1/',(c)=>{
-
-  return c.text("user signup")
-})
-
-
-app.post('/api/v1/signin',(c)=>{
-  return c.text("user signim")
-})
-
-
-app.post('/api/v1/blog',(c)=>{
-  return c.text("post blog")
-})
-app.put('/api/v1/blog/:id',(c)=>{
-  return c.text("put blog")
-})
-app.put('/api/v1/blog/bulk',(c)=>{
-  return c.text("put bulk")
-})
-export default app
+app.post("/api/v1/blog", (c) => {
+  return c.text("post blog");
+});
+app.put("/api/v1/blog/:id", (c) => {
+  return c.text("put blog");
+});
+app.put("/api/v1/blog/bulk", (c) => {
+  return c.text("put bulk");
+});
+export default app;
